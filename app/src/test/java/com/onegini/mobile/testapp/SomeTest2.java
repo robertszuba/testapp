df629690904ff1f417a99e2478e84a5364d1541b
Robert Szuba <robert@onegini.com>
2018-10-10 09:52:47 +0200

    first commit

---
+++ app/src/test/java/com/onegini/mobile/testapp/SomeTest2.java
@@ -0,0 +1,112 @@
+package com.onegini.mobile.testapp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SomeTest2 {
+
+  @Test
+  public void test_1() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_2() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_3() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_4() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_5() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_6() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_7() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_8() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_9() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_10() {
+    assertThat(true).isTrue();
+  }
+
+
+  @Test
+  public void test_11() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_12() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_13() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_14() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_15() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_16() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_17() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_18() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_19() {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  public void test_20() {
+    assertThat(true).isTrue();
+  }
+}
