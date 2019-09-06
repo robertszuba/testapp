74052cceeaa998c85a98d72f64f4cc65bb4a5361
Robert Szuba <robert@onegini.com>
2019-09-06 15:13:18 +0200

    Use Pattern it unit test

--- app/src/main/java/com/onegini/mobile/testapp/MyClass.java
+++ app/src/main/java/com/onegini/mobile/testapp/MyClass.java
@@ -4,13 +4,19 @@ import java.security.SecureRandom;
 
 public class MyClass {
 
+  private static final char[] VALID_CHARACTERS = "0123456879".toCharArray();
+
   private final SecureRandom secureRandom;
 
   MyClass(final SecureRandom secureRandom) {
     this.secureRandom = secureRandom;
   }
 
-  public int random() {
-    return secureRandom.nextInt(10);
+  public String random(final int length) {
+    char[] stringBuffer = new char[length];
+    for (int i = 0; i < length; i++) {
+      stringBuffer[i] = VALID_CHARACTERS[secureRandom.nextInt(10)];
+    }
+    return new String(stringBuffer);
   }
 }
--- app/src/test/java/com/onegini/mobile/testapp/MyClassTest.java
+++ app/src/test/java/com/onegini/mobile/testapp/MyClassTest.java
@@ -3,6 +3,8 @@ package com.onegini.mobile.testapp;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.security.SecureRandom;
+import java.util.Random;
+import java.util.regex.Pattern;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,17 +16,22 @@ public class MyClassTest {
 
   private SecureRandom secureRandom;
   private MyClass myClass;
+  private int expectedLength;
 
   @Before
   public void setup() throws Exception {
     secureRandom = SecureRandom.getInstance("SHA1PRNG");
     myClass = new MyClass(secureRandom);
+
+    final Random random = new Random();
+    expectedLength = random.nextInt(200) + 1; // [1 - 200]
   }
 
   @Test
   public void test() {
-    final int random = myClass.random();
-    assertThat(random).isLessThan(10);
-    assertThat(random).isGreaterThanOrEqualTo(0);
+    final String testedString = myClass.random(expectedLength);
+    final Pattern expectedPattern = Pattern.compile("^[0-9]{" + expectedLength + "}$");
+
+    assertThat(expectedPattern.matcher(testedString).find()).isTrue();
   }
 }
